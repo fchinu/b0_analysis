e9740ade3e87c3aa6e4f9414f578207bae8c40f1
fchinu <fachi2001@gmail.com>
2025-02-06 17:43:22 +0100

    Add simulations for DK decays

---
+++ simulations/b_to_dka/create_dummy_dataset.py
@@ -0,0 +1,28 @@
+import pandas as pd 
+import ROOT
+
+SIZE = 100000
+
+file = ROOT.TFile.Open('simulations/b_to_dka/MDPi_511_from_DKa_decay.root')
+tree = file.Get('treeBtoDKa')
+
+pt_mins = [1, 2, 4, 6, 8, 10, 14]
+pt_maxs = [2, 4, 6, 8, 10, 14, 23.5]
+
+df = pd.DataFrame(columns=['fM', 'fPt', 'ML_output'])
+for pt_min, pt_max in zip(pt_mins, pt_maxs):
+    tree.Draw('mBSmeared >> h_mb_smeared(760,4.9,5.66)', f'pTB>{pt_min} && pTB<{pt_max}', 'goff')
+    h_mb_smeared = ROOT.gDirectory.Get('h_mb_smeared')
+
+    mass = []
+    for i in range(SIZE):
+        mass.append(h_mb_smeared.GetRandom())
+    pt = [0.5 * (pt_min + pt_max)] * SIZE
+    ml_score = [1] * SIZE
+    b_mother = [511] * SIZE
+    c_mother = [411+321] * SIZE
+    df = pd.concat([df, pd.DataFrame({'fM': mass, 'fPt': pt,
+                                    'ML_output': ml_score, 'fPdgCodeBeautyMother': b_mother, 'fPdgCodeCharmMother': c_mother})])
+
+df.to_parquet("simulations/b_to_dka/dummy_for_template.parquet")
+    
